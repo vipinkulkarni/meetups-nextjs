b9d204164220eb7cb2b41b4a41b9f335151c9510
Vipinkumar K <vipinkulkarni16@gmail.com>
2022-03-27 11:35:38 +0530

    ready for deployment

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,71 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+import { MongoClient, ObjectId } from "mongodb";
+import { Fragment } from "react";
+import Head from "next/head";
+
+function MeetupDetails(props) {
+  return (
+    <Fragment>
+      <Head>
+        <title>{props.meetupData.title}</title>
+        <meta name="description" content={props.meetupData.description} />
+      </Head>
+      <MeetupDetail
+        image={props.meetupData.image}
+        title={props.meetupData.title}
+        address={props.meetupData.address}
+        description={props.meetupData.description}
+      />
+    </Fragment>
+  );
+}
+
+export async function getStaticPaths() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://meetups:zZhroJOXu6dj1GAg@cluster0.tn0ul.mongodb.net/meets-nextjs?retryWrites=true&w=majority"
+  );
+
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+
+  client.close();
+  return {
+    paths: meetups.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() },
+    })),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  const client = await MongoClient.connect(
+    "mongodb+srv://meetups:zZhroJOXu6dj1GAg@cluster0.tn0ul.mongodb.net/meets-nextjs?retryWrites=true&w=majority"
+  );
+
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetup = await meetupsCollection.findOne({ _id: ObjectId(meetupId) });
+
+  client.close();
+
+  return {
+    props: {
+      meetupData: {
+        id: meetup._id.toString(),
+        title: meetup.title,
+        description: meetup.description,
+        image: meetup.image,
+        address: meetup.address,
+      },
+    },
+  };
+}
+
+export default MeetupDetails;
